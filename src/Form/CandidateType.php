7c9c3f5e953ae6120b63b226fb058d76f1bba219
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2022-09-20 11:28:40 +0200

    edit candidate profile

---
+++ src/Form/CandidateType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Candidate;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+
+
+class CandidateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            // ->add('roles')
+            // ->add('password')
+            // ->add('isVerified')
+            // ->add('isActivated')
+            ->add('firstname')
+            ->add('lastname')
+            // ->add('cv')
+            ->add('cv', FileType::class, [
+                'label' => 'CV (fichier PDF)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez choisir un document au format PDF.',
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Candidate::class,
+        ]);
+    }
+}
