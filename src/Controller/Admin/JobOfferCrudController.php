013a8b3b2ced3e049f47fcead72355aabf8f6a42
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2022-09-28 17:13:01 +0200

    config easyadmin

--- src/Controller/Admin/JobOfferCrudController.php
+++ src/Controller/Admin/JobOfferCrudController.php
@@ -5,10 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\JobOffer;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-
 
 class JobOfferCrudController extends AbstractCrudController
 {
@@ -24,16 +24,28 @@ class JobOfferCrudController extends AbstractCrudController
             ->setEntityLabelInSingular('Offre d\'emploi')
         ;
     }
-
     
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id')
-                ->hideOnForm(),
-            TextField::new('title'),
-            TextField::new('city'),
-            TextEditorField::new('description'),
+                ->setLabel('Id')
+                ->hideWhenCreating()
+                ->setFormTypeOption('disabled', 'disabled'),
+            AssociationField::new('recruiter')
+                ->setLabel('Recruteur')
+                ->setFormTypeOption('choice_label', 'email')
+                ->hideWhenUpdating(),
+            TextField::new('recruiter.email')
+                ->setLabel('Recruteur')
+                ->onlyWhenUpdating()
+                ->setFormTypeOption('disabled', 'disabled'),
+            TextField::new('title')
+                ->setLabel('Intitulé'),
+            TextField::new('city')
+                ->setLabel('Ville'),
+            TextEditorField::new('description')
+                ->setLabel('Description'),
         ];
     }
     
